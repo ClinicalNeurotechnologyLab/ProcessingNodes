9f945787fdd109210b66974641c7f92e815e1cf1
Niels Peekhaus <niels.peekhaus@charite.de>
2025-02-24 18:11:10 +0100

    Add Github action for automated testing.

--- processingnodes/__init__.py
+++ processingnodes/__init__.py
@@ -1,10 +1,4 @@
-"""
-This is a package to test python packaging
-"""
-
 __version__ = "0.1.0"
-__author__ = "Niels Peekhaus"
-__credits__ = ""
 
 from .DataProcessor import DataProcessor
 from .ProcessingNode import ProcessingNode
